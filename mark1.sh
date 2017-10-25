bf369a6a005e9d26b299ccb2f82048904a5d0393
Vegetable <30365975+Loveeatapple@users.noreply.github.com>
2017-10-25 20:56:51 +0800

    Update mark1.sh
    developed by Issei Hyodo

--- mark1.sh
+++ mark1.sh
@@ -1,45 +1,27 @@
 #!/bin.sh
-
-#Check OS
-if [ -f /etc/redhat-release ];then
-        OS='CentOS'
-    elif [ ! -z "`cat /etc/issue | grep bian`" ];then
-        OS='Debian'
-    elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ];then
-        OS='Ubuntu'
-    else
-        echo "暂不支持您的系统。"
-        exit 1
+# 判断使用哪种包管理器
+if [ -n "$(command -v yum)" ]; then
+    pkg_mgr='yum'
+elif [ -n "$(command -v apt-get)" ]; then
+    pkg_mgr='apt'
+else
+    echo "yum 和 apt 都不可用，放弃安装！"
+  exit 1
 fi
 
-#install 
-if OS='CentOS'
-	yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-	yum install sslh
-else 
-apt-get install sslh
+# 安装 sslh 顺便获取本机 IP
+if [ "$pkg_mgr" == "yum" ]; then
+    my_ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+    yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm sslh || { echo "安装 sslh 失败！请查看错误信息。"; exit 1; }
+else
+    my_ip=$(/sbin/ip -o -4 addr list ens3 | awk '{print $4}' | cut -d/ -f1)
+    apt install -y sslh || { echo "安装 sslh 失败,请查看错误信息。"; exit 1; }
 fi
 
-#Configure Server-port
-read -t 60 -p "请输入本机的IP地"your_ip
-echo -e "\n"
-echo "IP为：$your_ip"
-cat << EOF > config
-listen:
-(
-    { host: "$your_ip"; port: "443"; }
-);
-EOF
+# 修改 sslh 配置文件的IP
+sed -i "s/thelonious/$my_ip/g" /etc/sslh.cfg
 
-rm /etc/sslh.cfg
-cat << EOF > /etc/sslh.cfg
-listen:
-(
-    { host: "$your_ip"; port: "443"; }
-);
-EOF
-
+# 应用 sslh 的新设置
 service sslh restart
-clear
 
 echo '配置完成'
